b97d5f0d6049f8002952ffe5061f38d14d36c353
alok <10aloksharma2004@gmail.com>
2024-05-26 22:47:56 +0530

    add template

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Card from '../components/Card'
+import './Home.css'
 
 export default function Home() {
   return (
-    <div>
-      home here
-    </div>
+    <>
+      <header className="navbar">
+
+        {/* guide for the developer */}
+        {/* created fields accoring to general overview of an ecommerce website */}
+        <div className="logo">logo</div>
+        <div className="searchbar">search</div>
+        <div className="cart">cart logo</div>
+      </header>
+      <main className="main">
+        products here
+        {/* guide for the developer */}
+        {/* render components from the source /components/cards */}
+        {/* <card/> <-- import card component */}
+        {/* reuse the card component for rendering multiple products like phone, clothes */}
+      </main>
+      <footer className="footer">
+        footer here
+      </footer>
+    
+    </>
   )
 }
